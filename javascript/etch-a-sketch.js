dcb731a715f5b6eacf87505c246a91ca8293779c
levib00 <levib8@hotmail.com>
2022-07-22 03:33:43 -0400

    allow Drawing and resizing grid

--- javascript/etch-a-sketch.js
+++ javascript/etch-a-sketch.js
@@ -1,11 +1,25 @@
-let squareDimensions = 600/16;
-let squareSquared = 16*16;
-
-for (let i =0; i < squareSquared; i++) {
-let gridSquare = document.createElement("div");
-gridSquare.style.width = `${squareDimensions}px`;
-gridSquare.style.height = `${squareDimensions}px`;
-gridSquare.style.color = "black";
-let screen = document.getElementById("screen");
-screen.appendChild(gridSquare);
+let gridSize = prompt("How Many Squares across would you like your grid to be? (max 100)")
+if (gridSize > 100) {
+    gridSize = prompt("How Many Squares across would you like your grid to be? (max 100)")
 }
+
+let squareDimensions = 960/gridSize;
+let squareSquared = gridSize*gridSize;
+
+for (let i = 0; i < squareSquared; i++) {
+    let gridSquare = document.createElement("div");
+    makeSquare(gridSquare)
+}
+
+function makeSquare(gridSquare) {
+    gridSquare.style.width = `${squareDimensions}px`;
+    gridSquare.style.height = `${squareDimensions}px`;
+    gridSquare.style.color = "black";
+    gridSquare.addEventListener("mouseenter", function() {
+        gridSquare.classList.add("squares")
+    })
+    let screen = document.getElementById("screen");
+    screen.appendChild(gridSquare);
+    
+}
+
